7a39c6f85770ca13d428d83d2032b5b5521f360d
yungzheng1412 <yungzheng90@gmail.com>
2025-11-17 22:43:30 +0800

    updated config file

--- core/config.py
+++ core/config.py
@@ -7,6 +7,6 @@ class Settings:
     WMS_API_KEY: str = os.getenv("WMS_API_KEY", "test123")
     MODEL_PATH: str = os.getenv("MODEL_PATH", "models/lstm_model.h5")
     HOST: str = os.getenv("HOST", "0.0.0.0")
-    PORT: int = int(os.getenv("PORT", "8000"))
+    PORT: int = int(os.getenv("PORT", os.getenv("RENDER_PORT", "8000")))
 
 settings = Settings()
